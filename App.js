3ef1a4bc310f099581e1209e6093034f4bccb68d
Poonyawat62114340243 <86709257+Poonyawat62114340243@users.noreply.github.com>
2021-07-14 15:54:50 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,120 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
- import React, { useState } from "react";
- import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { gray, red, rgb } from "chalk";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View,TextInput, Button, Picker} from "react-native";
  
  const FlexDirectionBasics = () => {
    const [flexDirection, setflexDirection] = useState("column");
- 
+   const [num1 , setA] = useState();
+   const [num2 , setB] = useState();
+   const [selectedValue, setSelectedValue] = useState("plus");
+   const calculate = () => {
+     if(selectedValue == "plus")
+     {
+       alert(parseFloat(num1)+parseFloat(num2))
+     }
+     else if(selectedValue == "minus")
+     {
+       alert(parseFloat(num1)-parseFloat(num2))
+     }
+     else if(selectedValue == "mul")
+     {
+       alert(parseFloat(num1)*parseFloat(num2))
+     }
+     else if(selectedValue == "div")
+     {
+       alert(parseFloat(num1)/parseFloat(num2))
+     }
+   }
+  
    return (
     <View style={{ flex:1, flexDirection: "column" }}>
-      <View style={{ flex:1, flexDirection: "row"}}>
-        <View style={{ flex: 1, backgroundColor: "blue",justifyContent:"flex-start",padding:10}}>
-          <View style={{width: 50, height:50, backgroundColor: "black"}}></View>
-        </View>
-        <View style={{ flex: 1,flexDirection: "row-reverse", backgroundColor: "green",padding:10}}>
-          <View style={{width: 50, height:50, backgroundColor: "black"}}></View>
-        </View>
-      </View>
-      <View style={{ flex:1, flexDirection: "row"}}>
-        <View style={{ flex: 1, backgroundColor: "yellow",flexDirection: "column-reverse",padding:10}}>
-          <View style={{width: 50, height:50, backgroundColor: "black"}}></View>
-        </View>
-        <View style={{ flex: 1, flexDirection:"column-reverse",backgroundColor: "red" ,padding:10}}>
-          <View style={{flexDirection:"row-reverse"}}>
-            <View style={{width: 50, height:50, backgroundColor: "black" }}></View>
-          </View>
-        </View>
-      </View>
-    
+      <TextInput style={{height:40,borderColor:red,borderWidth:1}}
+        placeholder="Input Your Text PLS !"
+        keyboardType = "number-pad"
+        onChangeText ={num1=>setA(num1)}
+      />
+      <TextInput style={{height:40,borderColor:red,borderWidth:1}}
+        placeholder="Input Your Text PLS !"
+        keyboardType = "number-pad"
+        onChangeText ={num2=>setB(num2)}
+      />
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height:50 }}
+        onValueChange={(itemValue,indexValue) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="+" value="plus"/>
+        <Picker.Item label="-" value="minus"/>
+        <Picker.Item label="*" value="mul"/>
+        <Picker.Item label="/" value="div"/>
+      </Picker>
+      <Button 
+        title="Learn More"
+        color="#2196F3"
+        accessibilityLabel="Learn more about this purple button"
+        onPress={calculate}
+      />
     </View>
 
    );
  };
- 
- const PreviewLayout = ({
-   label,
-   children,
-   values,
-   selectedValue,
-   setSelectedValue,
- }) => (
-   <View style={{ padding: 10, flex: 1 }}>
-     <Text style={styles.label}>{label}</Text>
-     <View style={styles.row}>
-       {values.map((value) => (
-         <TouchableOpacity
-           key={value}
-           onPress={() => setSelectedValue(value)}
-           style={[
-             styles.button,
-             selectedValue === value && styles.selected,
-           ]}
-         >
-           <Text
-             style={[
-               styles.buttonLabel,
-               selectedValue === value && styles.selectedLabel,
-             ]}
-           >
-             {value}
-           </Text>
-         </TouchableOpacity>
-       ))}
-     </View>
-     <View style={[styles.container, { [label]: selectedValue }]}>
-       {children}
-     </View>
-   </View>
- );
- 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     marginTop: 8,
-     backgroundColor: "aliceblue",
-   },
-   box: {
-     width: 50,
-     height: 50,
-   },
-   row: {
-     flexDirection: "row",
-     flexWrap: "wrap",
-   },
-   button: {
-     paddingHorizontal: 8,
-     paddingVertical: 6,
-     borderRadius: 4,
-     backgroundColor: "oldlace",
-     alignSelf: "flex-start",
-     marginHorizontal: "1%",
-     marginBottom: 6,
-     minWidth: "48%",
-     textAlign: "center",
-   },
-   selected: {
-     backgroundColor: "coral",
-     borderWidth: 0,
-   },
-   buttonLabel: {
-     fontSize: 12,
-     fontWeight: "500",
-     color: "coral",
-   },
-   selectedLabel: {
-     color: "white",
-   },
-   label: {
-     textAlign: "center",
-     marginBottom: 10,
-     fontSize: 24,
-   },
- });
  
  export default FlexDirectionBasics;
